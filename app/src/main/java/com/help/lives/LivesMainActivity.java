11e244ffc87ff452ec7a13f1cb3a3cdf4e75383d
shichangying <changying03@163.com>
2017-07-25 15:05:29 +0800

    Initial commit

---
+++ app/src/main/java/com/help/lives/LivesMainActivity.java
@@ -0,0 +1,13 @@
+package com.help.lives;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LivesMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lives_main);
+    }
+}
