55b04ecafb47fa098e4b85f4dca2589b60504d97
shichangying <changying03@163.com>
2017-07-25 15:08:40 +0800

    测试。

--- app/src/main/java/com/help/lives/LivesMainActivity.java
+++ app/src/main/java/com/help/lives/LivesMainActivity.java
@@ -1,7 +1,9 @@
 package com.help.lives;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LivesMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class LivesMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lives_main);
+        Log.d("33", "onCreate: ");
     }
 }
